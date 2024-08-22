3ea814b5b5063dc98d73439b8a614b715ad18d92
seeratfatema <115491132+seeratfatema@users.noreply.github.com>
2024-08-22 12:32:05 +0530

    links added to frontend

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -7,7 +7,7 @@ export default function MyOrder() {
 
     const fetchMyOrder = async () => {
         console.log(localStorage.getItem('userEmail'))
-        await fetch("http://localhost:5000/api/myOrderData", {
+        await fetch("https://foodeez.onrender.com/api/myOrderData", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
